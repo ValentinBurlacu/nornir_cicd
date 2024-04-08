2c9b23a2362a94404b6b53f2bc3e18f86a9c070d
Valentin Burlacu <valentin.burlacu@ymail.com>
2024-04-08 15:13:41 +0100

    added testing_script.py file

---
+++ testing_script.py
@@ -0,0 +1,25 @@
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_scrapli.tasks import send_command
+
+cf = "/Volumes/t7-2tb-blue/files/_drive_dropbox_files/Dropbox/develop/net_auto/_config.yaml"
+nr = InitNornir(config_file=cf)
+
+def pull_info(task):
+    result = task.run(task=send_command, command="show ip ospf nei")
+    task.host['facts'] = result.scrapli_response.genie_parse_output()
+    interfaces = task.host['facts']['interfaces']
+    for int in interfaces:
+        neighbors = interfaces[int]["neighbors"]
+        for neighbor in neighbors:
+            state = neighbors[neighbor]['state']
+            return state
+
+
+state_result = nr.run(task=pull_info)
+for host in nr.inventory.hosts.values():
+    # print(host)
+    state = state_result[f"{host}"][0].result
+    # print(state)
+    assert "FULL" in state, 'Failed'
+print("PASSED")
